621e875cac600994164e89f0c73316bd676542bb
Gavin Waite <gavwaite@gmail.com>
2020-01-22 18:38:00 -0500

    Added basic navigation and tests

--- helloDjango/boards/tests.py
+++ helloDjango/boards/tests.py
@@ -5,14 +5,23 @@ from .models import Board
 # Create your tests here.
 
 class HomeTests(TestCase):
-    def test_home_view_status_code(self):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
         url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    # The assert contains checks to see if the response (the page html) includes
+    # given text - in this case href="boards/1" or a link to board with pk = 1
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
 
 class BoardTopicTests(TestCase):
 # The setUp method allows us to populate the Django database before running tests as
@@ -34,3 +43,9 @@ class BoardTopicTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
